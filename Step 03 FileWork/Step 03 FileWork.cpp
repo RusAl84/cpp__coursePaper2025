75102be1363c1bc7b647b363ed345bedf59ad773
Rusakov Aleksey <rusal@bk.ru>
2025-06-02 23:36:19 +0300

    finish Step 03 FileWork

--- Step 03 FileWork/Step 03 FileWork.cpp
+++ Step 03 FileWork/Step 03 FileWork.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <Windows.h>
+#include <fstream>
 #include "..//Step 01 StructWork/Departament.h"
 #include "..//Step 02 ListWork/DepartamentList.h"
 
@@ -39,7 +40,56 @@ public:
         }
         fclose(TextFile);
     }
+    void loadData(DepartamentList* depList) {
+        int numStud = 0;
+        string line;
+        ifstream TextFile(filename);
+        int countItems = 0;
+        class Departament* dp ;
+        //dp->inst = "ИКБ";
+        //dp->name = "КБ-2";
+        //dp->position = "ассистент";
+        //dp->resp_person = "Брежнева Е.Д.";
+        struct Item* item;
+        //item->title = "Дрель";
+        //item->inventory_number = "434006913";
+        //item->commissioning_date.setDate(28, 05, 2025);
+        //item->service_life = 36;
+        while (getline(TextFile, line)) {
+            dp = new Departament();
+            dp->data->inst = line;
+            getline(TextFile, line);
+            dp->data->name = line;
+            getline(TextFile, line);
+            dp->data->position = line;
+            getline(TextFile, line);
+            dp->data->resp_person = line;
+            getline(TextFile, line);
+            countItems=atoi(line.c_str());
+            for (int i = 0; i < countItems; i++) {
+                item = new Item();
+                getline(TextFile, line);
+                item->title = line;
+                getline(TextFile, line);
+                item->inventory_number = line;
+                getline(TextFile, line);
+                item->commissioning_date.day = atoi(line.c_str());
+                getline(TextFile, line);
+                item->commissioning_date.month = atoi(line.c_str());
+                getline(TextFile, line);
+                item->commissioning_date.year = atoi(line.c_str());
+                getline(TextFile, line);
+                item->service_life = atoi(line.c_str());
+                dp->data->items.push_back(*item);
+            }
+            depList->addItem(dp);
+        }
+        TextFile.close();
+    }
+
 };
+
+
 
 int main()
 {
@@ -53,6 +103,9 @@ int main()
     class FileWork* fWork = new FileWork();
     fWork->saveData(depList->myHead);
     DepartamentList* depList_new = new  DepartamentList();
-
+    
+    fWork->loadData(depList_new);
+    cout << endl << endl << endl << "depList_new";
+    depList_new->Dislay();
 }
 
