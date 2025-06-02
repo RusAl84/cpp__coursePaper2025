fd0f47803c1bdb951a72d2ecb2959595b27e2a6e
Rusakov Aleksey <rusal@bk.ru>
2025-06-03 00:14:51 +0300

    fix

--- Step 04 UIWork/Step 04 UIWork.cpp
+++ Step 04 UIWork/Step 04 UIWork.cpp
@@ -1,8 +1,22 @@
-﻿
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+#include "..//Step 03 FileWork/FileWork.h"
+#include "..//Step 01 StructWork/Departament.h"
+#include "..//Step 02 ListWork/DepartamentList.h"
 
-#include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    cout << "Step 04 UIWork\n";
+    DepartamentList* depList = new  DepartamentList();
+    class FileWork* fWork = new FileWork();
+    fWork->loadData(depList);
+    //cout << endl << endl << endl << "depList_new";
+    //depList->Dislay();
+
+
+
 }
